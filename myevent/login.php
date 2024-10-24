07966dab452774646a0d265a55f7201682e68413
Ahmad Hanis <36687333+ahmadhanis@users.noreply.github.com>
2024-10-24 16:24:33 +0800

    login + registration

--- myevent/login.php
+++ myevent/login.php
@@ -1,3 +1,26 @@
+<?php
+if (isset($_POST['submit'])) {
+    $email = $_POST['useremail'];
+    $password = sha1($_POST['password']);
+    $sqllogin = "SELECT  `admin_email`, `admin_password` FROM `tbl_admins` WHERE `admin_email` = '$email' AND `admin_password` = '$password'";
+    
+    try{
+        include("dbconnect.php"); // database connection
+        $stmt = $conn->prepare($sqllogin);
+        $stmt->execute();
+        $number_of_rows = $stmt->rowCount();
+        if ($number_of_rows > 0) {
+            echo "<script>alert('Success')</script>";
+            echo "<script>window.location.replace('mainpage.php')</script>";
+        }else{
+            echo "<script>alert('Failed')</script>";
+        }
+    }catch(PDOException $e){
+        echo "<script>alert('Failed!!!')</script>";
+    }
+}
+?>
+
 <html>
 
 <head>
@@ -6,7 +29,7 @@
     <link rel="stylesheet" type="text/css" href="https://www.w3schools.com/w3css/4/w3.css">
 </head>
 
-<body>
+<body onload="loadData()">
     <header class="w3-center w3-padding-32 w3-teal">
         <div class="w3-margin">
             <h1>Slumberjer Event Sdn Bhd</h1>
@@ -22,9 +45,14 @@
                 placeholder="Enter Email" required><br>
             <input class="w3-input w3-round w3-border" type="password" id="passwordid" name="password"
                 placeholder="Enter Password" required><br>
-            <p class="">Remember Me &nbsp;&nbsp;&nbsp;<input type="checkbox" name="rememberme" id="checkboxid"></p>
-            <input class="w3-input w3-round w3-button w3-teal" type="submit" value="Login">
+            <p class="">Remember Me &nbsp;&nbsp;&nbsp;
+                <input type="checkbox" name="rememberme" id="checkboxid" onclick="rememberMe()">
+            </p>
+            <input class="w3-input w3-round w3-button w3-teal" type="submit" name="submit" value="Login">
         </form>
+    </div>
+    <div class="w3-center w3-container">
+        <a href="registration.php">Register</a>
     </div>
     <div class="" style="height:200px">
     </div>
@@ -34,5 +62,33 @@
         </p>
     </footer>
 </body>
+
+<script>
+function rememberMe() {
+    if (document.getElementById('checkboxid').checked) {
+        var useremail = document.getElementById('useremailid').value;
+        var password = document.getElementById('passwordid').value;
+        var remember = document.getElementById('checkboxid').value;
+        localStorage.setItem('useremail', useremail);
+        localStorage.setItem('password', password);
+        localStorage.setItem('remember', remember);
+        alert('Success');
+    } else {
+        localStorage.removeItem('useremail');
+        localStorage.removeItem('password');
+        localStorage.removeItem('remember');
+        document.getElementById('useremailid').value = '';
+        document.getElementById('passwordid').value = '';
+        document.getElementById('checkboxid').checked = false;
+        alert('Removed')
+    }
+}
+
+function loadData() {
+    document.getElementById('useremailid').value = localStorage.getItem('useremail');
+    document.getElementById('passwordid').value = localStorage.getItem('password');
+    document.getElementById('checkboxid').checked = localStorage.getItem('remember');
+}
+</script>
 
 </html>
---
+++ myevent/registration.php
@@ -0,0 +1,59 @@
+<?php
+
+if (isset($_POST['submit'])) {
+    $email = $_POST['useremail'];
+    $password = sha1($_POST['password']);
+    $sqlregister = "INSERT INTO `tbl_admins`(`admin_email`, `admin_password`) VALUES ('$email','$password')";
+    
+    try{
+        include("dbconnect.php"); // database connection
+        $conn->query($sqlregister);
+        echo "<script>alert('Success')</script>";
+        echo "<script>window.location.replace('login.php')</script>";
+    }catch(PDOException $e){
+        echo "<script>alert('Failed!!!')</script>";
+    }
+}
+
+?>
+
+<html>
+
+<head>
+    <title>Login</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="https://www.w3schools.com/w3css/4/w3.css">
+</head>
+
+<body>
+    <header class="w3-center w3-padding-32 w3-teal">
+        <div class="w3-margin">
+            <h1>Slumberjer Event Sdn Bhd</h1>
+            <h3>Your One Stop Event Manager</h3>
+        </div>
+    </header>
+    <div class="w3-hide-small" style="height:100px">
+    </div>
+    <div class="w3-container" style="max-width: 600px;margin:auto">
+        <h1>Register</h1>
+        <form action="registration.php" method="post">
+            <input class="w3-input w3-round w3-border" type="text" id="useremailid" name="useremail"
+                placeholder="Enter Email" required><br>
+            <input class="w3-input w3-round w3-border" type="password" id="passwordid" name="password"
+                placeholder="Enter Password" required><br>
+            <input class="w3-input w3-round w3-button w3-teal" type="submit" name="submit" value="Register">
+        </form>
+    </div>
+    <div class="w3-center w3-container">
+        <a href="login.php">Login</a>
+    </div>
+    <div class="" style="height:200px">
+    </div>
+    <footer class="w3-container w3-grey">
+        <p style="text-align: center">
+            Copyright &copy; 2023 Slumberjer Event Sdn Bhd
+        </p>
+    </footer>
+</body>
+
+</html>
